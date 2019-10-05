152f9de8d97528174a235a4da2fdb44c5c41ac83
ozgurkaracam <ozgurkrcm@hotmail.com>
2019-10-05 16:05:35 +0300

    arragments

--- app/src/main/java/com/example/veterineruygulamas/Activities/MainActivity.java
+++ app/src/main/java/com/example/veterineruygulamas/Activities/MainActivity.java
@@ -1,7 +1,10 @@
-package com.example.veterineruygulamas;
+package com.example.veterineruygulamas.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import com.example.veterineruygulamas.R;
 
 public class MainActivity extends AppCompatActivity {
 
