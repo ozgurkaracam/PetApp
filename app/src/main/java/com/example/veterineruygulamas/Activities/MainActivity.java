1be6a3c73c8a55c8e29623eaf622508e78ad57a4
ozgurkaracam <ozgurkrcm@hotmail.com>
2019-10-09 12:21:05 +0300

    main activity.

--- app/src/main/java/com/example/veterineruygulamas/Activities/MainActivity.java
+++ app/src/main/java/com/example/veterineruygulamas/Activities/MainActivity.java
@@ -13,21 +13,11 @@ import com.example.veterineruygulamas.R;
 import com.example.veterineruygulamas.Utils.Auth;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
     Auth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         auth=new Auth(MainActivity.this);
-        textView=findViewById(R.id.textView);
-        textView.setText(auth.getUsername());
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                auth.outAuth();
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            }
-        });
     }
 }
