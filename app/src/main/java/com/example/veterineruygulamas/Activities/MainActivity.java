52a5c29322d15c63a2cb5fd22f9c27275b38657e
ozgurkaracam <ozgurkrcm@hotmail.com>
2019-10-24 09:11:41 +0300

    Added Call Us

--- app/src/main/java/com/example/veterineruygulamas/Activities/MainActivity.java
+++ app/src/main/java/com/example/veterineruygulamas/Activities/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.veterineruygulamas.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,9 +21,10 @@ import com.example.veterineruygulamas.Utils.Auth;
 
 public class MainActivity extends AppCompatActivity {
     Auth auth;
-    CardView sanalKarneler,soruSorCardView,takvimCardView,cardViewDuyuru;
+    CardView sanalKarneler,soruSorCardView,takvimCardView,cardViewDuyuru,cardViewCallUs;
     Toolbar toolbar;
     TextView cikisyaptext,hosgeldiniztext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         hosgeldiniztext=findViewById(R.id.hosgeldiniztext);
         takvimCardView=findViewById(R.id.takvimCardView);
         cardViewDuyuru=findViewById(R.id.cardViewDuyuru);
+        cardViewCallUs=findViewById(R.id.cardViewCallUs);
         hosgeldiniztext.setText("Hoşgeldiniz :"+auth.getUsername());
     }
     private void authcontrol(){
@@ -74,6 +81,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,DuyuruActivity.class));
+            }
+        });
+        cardViewCallUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:+905363475667"));
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},1);
+                }
+                else{
+                    startActivity(intent);
+                }
+
             }
         });
 
